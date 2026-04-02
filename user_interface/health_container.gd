1709cb8cc897196f6bfde13fe545ec4f75718b17
Nicholas Huffman <njh102304@gmail.com>
2026-04-02 19:20:43 -0400

    componentsHookedUp

--- user_interface/health_container.gd
+++ user_interface/health_container.gd
@@ -1,9 +1,9 @@
-extends Panel
+extends Sprite3D
 
 func _healthChanged(old_health: float, new_health: float) -> void:
 	visible = true;
-	get_child(0).value = new_health
+	%HealthContainer.value = new_health
 
 
 func _maxHealthChanged(old_max: float, new_max: float) -> void:
-	get_child(0).max_value = new_max
+	%HealthContainer.max_value = new_max
