1a9078d1eb54f30a536e61be4acb8d4fcb64902b
Nicholas Huffman <njh102304@gmail.com>
2026-04-02 20:41:55 -0400

    firstSceneIntegrated

--- user_interface/health_container.gd
+++ user_interface/health_container.gd
@@ -1,9 +1,9 @@
-extends Sprite3D
+extends Panel
 
 func _healthChanged(old_health: float, new_health: float) -> void:
 	visible = true;
-	%HealthContainer.value = new_health
+	get_child(0).value = new_health
 
 
 func _maxHealthChanged(old_max: float, new_max: float) -> void:
-	%HealthContainer.max_value = new_max
+	get_child(0).max_value = new_max
